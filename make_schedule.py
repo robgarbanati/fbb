4e50ee1bd13e71ef984df195215d7b676f7da596
Robert Garbanati <robert.garbanati@automotivepower.com>
2023-10-26 19:32:11 -0700

    doing an ok job getting both schedules

--- make_schedule.py
+++ make_schedule.py
@@ -7,7 +7,7 @@ import common
 import sys
 
 def make_schedule(gm_name: str, source_file: str):
-    with open('rosters/my_schedule.raw', 'r') as rawfile:
+    with open(source_file, 'r') as rawfile:
         data = rawfile.readlines()
 
         #  lines_iter = iter(rawfile)
@@ -19,7 +19,7 @@ def make_schedule(gm_name: str, source_file: str):
         # found_name = False
         name = ""
         out_text = ""
-        move_found = False
+        counting_games = False
         games_left_file = open(f'rosters/{gm_name}_roster.csv'.format(n=name), 'w')
         games_left_file.write("0,1,2\n")
         # skip 5 lines
@@ -30,44 +30,57 @@ def make_schedule(gm_name: str, source_file: str):
             num_spaces = line.count(' ')
             # state 1: found doublename. Save previous player to file if there was one.
             if num_spaces == 2:
-                if name != "":
-                    print(f"found 2 spaces. Going to write: players[{name}] = {players[name]}")
-                    games_left_file.write(",{name},{num_games}\n".format(name=name, num_games=players[name]))
                 doublename = True
                 print(f"found doublename")
                 # found_name = False
-                name = ""
-                move_found = False
                 prev_line = line
                 continue
             # state 2: found name. Save name.
-            if num_spaces == 1 and doublename == True and line in prev_line:
-                doublename = False
+            if num_spaces == 1 and doublename == True and line in prev_line and name == "":
                 # found_name = True
                 name = line
                 print(f"found player: {name=}")
                 players[name] = 0
                 continue
-            # state 3: found move.
-            if line == "MOVE" or line == "O":
-                move_found = True
-                print(f"move_found")
+            # state 3 (optional): Found out status
+            if line == "O":
+                # players[name] = -2
+                out_text = "O"
+                continue
+            # state 4: Found position. Start counting games.
+            if counting_games == False and name != "" and \
+                    ("SG" in line or \
+                    "PG" in line or \
+                    "SF" in line or \
+                    "PF" in line or \
+                    line == "C"):
+                counting_games = True
+                print(f"counting_games")
                 if line == "O":
                     players[name] = -2
                     out_text = "O"
                 continue
-            # state 4: Increment player's game countfor every line that is not "--"
-            if move_found and num_spaces == 0 and line != "--" \
-                    and name != "" and "SG" not in line and \
-                    "PG" not in line and "SF" not in line and \
-                    "PF" not in line and len(line) > 1 and \
-                    "UTIL" not in line and "Bench" not in line \
-                    and "Empty" not in line and "IR" not in line:
-            # if move_found and num_spaces == 0 and name != "" and len(line) > 2:
+            # state 6: End of player's schedule. Reset state.
+            if counting_games and \
+                    (line == "SG" or line == "PG" or line == "SF" or \
+                    line == "PF" or line == "C" or line == "UTIL" or \
+                    line == "Bench" or line == "IR"):
+                print(f"found positional placement. Going to write: {out_text},{name},{players[name]}")
+                # games_left_file.write(",{name},{num_games}\n".format(name=name, num_games=players[name]))
+                games_left_file.write(f"{out_text},{name},{players[name]}\n")
+                counting_games = False
+                name = ""
+                out_text = ""
+                continue
+            # state 5: Increment player's game countfor every line that is not "--"
+            if counting_games and line != "--" and num_spaces == 0 and \
+                    line != "MOVE" and line != "":
+                # assert num_spaces == 0, "unexpected line: {line}".format(line=line)
                 players[name] += 1
                 print(f"players[{name}] = {players[name]}")
-        print(f"End of loop. Going to write: players[{name}] = {players[name]}")
-        games_left_file.write(f"{out_text},{name},{players[name]}\n")
+
+        # print(f"End of loop. Going to write: players[{name}] = {players[name]}")
+        # games_left_file.write(f"{out_text},{name},{players[name]}\n")
 
 def check_if_file_exists(infile):
     if not path.exists(infile):
@@ -81,7 +94,7 @@ def calc_my_and_their_games_left(other_team_name):
     check_if_file_exists("rosters/their_schedule.raw")
 
     make_schedule("rob", "rosters/my_schedule.raw")
-    make_schedule(other_team_name, f"rosters/{other_team_name}_schedule.raw")
+    make_schedule(other_team_name, "rosters/their_schedule.raw")
 
 if __name__ == '__main__':
     calc_my_and_their_games_left()
