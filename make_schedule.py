fe5cc676c6d50af8547b110938be87b36ecffd44
Robert Garbanati <robert.garbanati@automotivepower.com>
2023-10-29 12:27:07 -0700

    mostly can make a full schedule. except Wendellcarterjr is a problem for some reason

--- make_schedule.py
+++ make_schedule.py
@@ -6,12 +6,9 @@ from os import path
 import common
 import sys
 
-def make_schedule(gm_name: str, source_file: str):
+def make_schedule(gm_name: str, source_file: str, last_day: str = "NOV 5"):
     with open(source_file, 'r') as rawfile:
         data = rawfile.readlines()
-
-        #  lines_iter = iter(rawfile)
-        clean_data = []
         lines_iter = iter(data[4:])
         prev_line = ""
         players = {}
@@ -19,35 +16,53 @@ def make_schedule(gm_name: str, source_file: str):
         # found_name = False
         name = ""
         out_text = ""
+        num_days = 0
         counting_games = False
+        last_day_found = False
+        days_counted_for_this_player = 0
         games_left_file = open(f'rosters/{gm_name}_roster.csv'.format(n=name), 'w')
         games_left_file.write("0,1,2\n")
-        # skip 5 lines
         for line in lines_iter:
+            # state 1: Ignore first few useless lines
+            if line == "SLOT" or line == "PLAYER" or line == "OPP" \
+                    or line == "STATUS" or line == "ACTION":
+                continue
+            # state 2: Count each date line until last_day
+            if line.startswith("OCT") or line.startswith("NOV") or line.startswith("DEC") or \
+                    line.startswith("JAN") or line.startswith("FEB") or line.startswith("MAR") or \
+                    line.startswith("APR"):
+                if  last_day_found:
+                    continue
+                num_days += 1
+                print(f"{num_days=}")
+            if line == last_day:
+                last_day_found = True
+                print(f"{num_days=}")
+                continue
             line = line.replace('\n', '')
             print(f'{line=}')
             # count number of spaces in line
             num_spaces = line.count(' ')
-            # state 1: found doublename. Save previous player to file if there was one.
+            # state 3: done with dates. find doublename.
             if num_spaces == 2:
                 doublename = True
                 print(f"found doublename")
                 # found_name = False
                 prev_line = line
                 continue
-            # state 2: found name. Save name.
+            # state 4: find name. Save name.
             if num_spaces == 1 and doublename == True and line in prev_line and name == "":
                 # found_name = True
                 name = line
                 print(f"found player: {name=}")
                 players[name] = 0
                 continue
-            # state 3 (optional): Found out status
+            # state 5: (optional): Find out status
             if line == "O":
                 # players[name] = -2
                 out_text = "O"
                 continue
-            # state 4: Found position. Start counting games.
+            # state 6: Find position. Start counting games.
             if counting_games == False and name != "" and \
                     ("SG" in line or \
                     "PG" in line or \
@@ -55,12 +70,13 @@ def make_schedule(gm_name: str, source_file: str):
                     "PF" in line or \
                     line == "C"):
                 counting_games = True
+                days_counted_for_this_player = 0
                 print(f"counting_games")
                 if line == "O":
-                    players[name] = -2
+                    # players[name] = -2
                     out_text = "O"
                 continue
-            # state 6: End of player's schedule. Reset state.
+            # state 8: End of player's schedule. Reset state.
             if counting_games and \
                     (line == "SG" or line == "PG" or line == "SF" or \
                     line == "PF" or line == "C" or line == "UTIL" or \
@@ -73,15 +89,19 @@ def make_schedule(gm_name: str, source_file: str):
                 name = ""
                 out_text = ""
                 continue
-            # state 5: Increment player's game countfor every line that is not "--"
-            if counting_games and line != "--" and num_spaces == 0 and \
-                    line != "MOVE" and line != "":
-                # assert num_spaces == 0, "unexpected line: {line}".format(line=line)
-                players[name] += 1
-                print(f"players[{name}] = {players[name]}")
+            # state 7: Increment player's game countfor every line that is not "--"
+            # if counting_games and line != "--" and num_spaces == 0 and \
+            #         line != "MOVE" and line != "":
+            if counting_games:
+                if num_spaces == 0 and line != "MOVE" and line != "":
+                    days_counted_for_this_player += 1
+                    print(f"{days_counted_for_this_player=}")
+                    if line != "--" and days_counted_for_this_player <= num_days:
+                        players[name] += 1
+                        print(f"players[{name}] = {players[name]}")
 
-        # print(f"End of loop. Going to write: players[{name}] = {players[name]}")
-        # games_left_file.write(f"{out_text},{name},{players[name]}\n")
+        print(f"End of loop. Going to write: players[{name}] = {players[name]}")
+        games_left_file.write(f"{out_text},{name},{players[name]}\n")
 
 def check_if_file_exists(infile):
     if not path.exists(infile):
